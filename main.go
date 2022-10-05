614119ae6634e71d35f9ba09617309986242ccf6
sxz799 <sxz15371@163.com>
2022-10-05 10:22:14 +0800

    添加地址显示

--- main.go
+++ main.go
@@ -5,7 +5,48 @@ import (
 	"goftp.io/server/core"
 	"goftp.io/server/driver/file"
 	"log"
+	"net"
 )
+
+func getIpFromAddr(addr net.Addr) net.IP {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip == nil || ip.IsLoopback() {
+		return nil
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return nil // not an ipv4 address
+	}
+	return ip
+}
+func GetLocalIP() {
+	interfaces, _ := net.Interfaces()
+	for _, iface := range interfaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+		for _, addr := range addrs {
+			ip := getIpFromAddr(addr)
+			if ip == nil {
+				continue
+			}
+			fmt.Println("ftp地址：ftp://" + ip.String())
+		}
+	}
+}
 
 func main() {
 
@@ -46,8 +87,10 @@ func main() {
 	}
 	// start ftp server
 	s := core.NewServer(opt)
+	GetLocalIP()
 	err := s.ListenAndServe()
 	if err != nil {
 		log.Fatal("服务启动失败:", err)
 	}
+
 }
